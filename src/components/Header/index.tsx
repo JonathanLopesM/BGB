ade684435758c0754e6b14dc57e0468683cfad30
Jonathan Lopes Machado <jonathan.lopes1004@gmail.com>
2021-09-13 00:53:39 -0300

    emergency

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,29 @@
 import styles from './header.module.scss';
 import Link from 'next/link';
 import { SignInButton } from '../signInButton/index';
+import { ActiveLink } from '../activeLink';
 
 export function Header(){
   return (
     
       <header className={styles.headerContainer}>
-        
         <div className={styles.headerContent}>
-
-        
           <Link href="/" passHref>
             <h1>BRAZIL GOLDEN BOYS</h1>
           </Link>
          <nav>
-           <Link href="/">HOME</Link>
-           <Link href="#">WHO WE ARE</Link>
-           <Link href="#">PLAYERS</Link>
-           <Link href="#">CONTACT</Link>
+           <ActiveLink activeClassName={styles.active} href="/">
+              <a >HOME</a>
+           </ActiveLink>
+           <ActiveLink activeClassName={styles.active} href="#">
+              <a>WHO WE ARE</a>
+            </ActiveLink>
+           <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+              <a >PLAYERS</a>
+           </ActiveLink>
+           <ActiveLink activeClassName={styles.active} href="#">
+              <a>CONTACT</a>
+           </ActiveLink>
          <SignInButton/>
          </nav>
       </div>
